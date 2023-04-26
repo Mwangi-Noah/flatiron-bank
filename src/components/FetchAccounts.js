8abbf3031ab2a4adb9d74ccf3a234663d0762cfa
Noah <noahmwangi@gmail.com>
2023-04-26 07:52:49 +0300

    I still can load db.json

--- src/components/FetchAccounts.js
+++ src/components/FetchAccounts.js
@@ -1,39 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import SearchFunction from "./SearchFunction";
 import MakeForm from "./MakeForm";
 
 function FetchAccounts() {
     //Creating the state for search input
     const [transaction, setTransaction] = useState([])
-    const [filteredResults, setFilteredResults] = useState([]);
-    const [searchInput, setSearchInput] = useState('');
-  
+    const [search, setSearch] = useState("");
     useEffect(() => {
-        fetch('db.json/transactions')
+        fetch('db.json/transactions?q=" + search')
           .then((resp) => resp.json())
           .then(transaction => setTransaction(transaction))
-      }, [])
+      }, [search])
+      console.log(transaction)
       //filter out our APIData using the filter method.
-      const searchItems = (searchValue) => {
-        setSearchInput(searchValue)
-        if (searchInput !== '') {
-            const filteredData = setTransaction.filter((item) => {
-                return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
-            })
-            setFilteredResults(filteredData)
-        }
-        else{
-            setFilteredResults(transaction)
-        }
+     
+function searchDatabase(e){
+    <input
+        type="text"
+        placeholder="Search your Recent Transactions"
+        onChange={setSearch(e.target.value)}
+    />
     }
-    
     return (
-        <div>
-        
-        <MakeForm transactions={filteredResults} />
-        </div>
+    <div>
+        <MakeForm transactions={transaction} />
+    </div>
     );
 }
+
 
 export default FetchAccounts;
 
